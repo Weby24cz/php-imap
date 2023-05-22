ab95f93d3f5a336afeb156fd7e335c8a8fec7368
pavel.kubicek <pavel.kubicek@movis.cz>
2023-05-22 13:59:27 +0200

    fix - duplicate attachment ID handling

--- src/Message.php
+++ src/Message.php
@@ -589,11 +589,7 @@ class Message {
         $oAttachment = new Attachment($this, $part);
 
         if ($oAttachment->getName() !== null && $oAttachment->getSize() > 0) {
-            if ($oAttachment->getId() !== null) {
-                $this->attachments->put($oAttachment->getId(), $oAttachment);
-            } else {
-                $this->attachments->push($oAttachment);
-            }
+            $this->attachments->put(md5($oAttachment->getName() . $oAttachment->getSize() . $oAttachment->getContent()), $oAttachment);
         }
     }
 
